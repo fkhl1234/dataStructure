bc698c6a372d7f23f7e0b733d68c397dc06639ce
fkhl1234 <fkhl1234@gmail.com>
2024-08-18 15:31:29 +0900

    0818

--- 2_commit.cpp
+++ 2_commit.cpp
@@ -1,9 +1,20 @@
-// Created on iPad.
-
+#include <array>
 #include <iostream>
-using namespace std;
+#include <type_traits>
+
+template<typename... Args>
+auto build_array(Args&&... args) -> std::array<typename std::common_type<Args...>::type, sizeof...(args)> {
+    using commonType = typename std::common_type<Args...>::type;
+    return {std::forward<commonType>((Args&&)args)...};
+}
 
 int main() {
-    cout << "Hello World!";
-    return 0;
+    auto data = build_array(1, 0u, 'a', 3.2f, false);
+
+    for(auto i:data) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << typeid(data).name() << std::endl;
 }
