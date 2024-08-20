c9bc83b3211f178156a99f299ac85c93a53117cd
PC_Isaac <fkhl1234@gmail.com>
2024-08-20 20:40:59 +0900

    0820

--- 3_forwardlist_removeif.cpp
+++ 3_forwardlist_removeif.cpp
@@ -3,5 +3,18 @@
 #include <forward_list>
 
 struct citizen {
-    
+    std::string name;
+    int age;
+};
+
+std::ostream &operator<<(std::ostream &os, const citizen &c) {
+    return (os << "[" << c.name << "," << c.age << "]");
+}
+
+int main() {
+    std::forward_list<citizen> citizens = { 
+        {"KIM", 22}, {"CHOI", 32}
+    };
+
+    auto citizens_copy = citizens;
 }
